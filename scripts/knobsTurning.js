76f135e6f8002555b7b6609fcf3ec1aeb12f896a
sumzulfikar <sumaiya.zulfikar29@gmail.com>
2024-07-14 15:39:44 +0100

    now user can use knobs to draw sketches

--- scripts/knobsTurning.js
+++
@@ -1,9 +0,0 @@
-const verticalKnob = document.getElementById("horizontal-knob");
-const horizontalKnob = document.getElementById("vertical-knob");
-
-const knobRotation = (knob, coordinates) => {
-    drawLine(coordinates.x, coordinates, y);
-
-
-
-}
