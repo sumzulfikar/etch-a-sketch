28f70e49f15016fb875f6ea410083ec2a0758e1c
sumzulfikar <sumaiya.zulfikar29@gmail.com>
2024-07-31 20:46:06 +0100

    shake and clear added

---
+++ scripts/shake.js
@@ -0,0 +1,21 @@
+
+
+const clearBtn = document.getElementById("clear");
+const shake = () => {
+
+    const device = document.getElementById("etch-device");
+
+    const canvas = document.getElementById("drawing-area");
+    const context = canvas.getContext("2d");
+    let tween = gsap.timeline()
+        .to(device, { rotation: -10, duration: 0.2, ease: "elatic" })
+        .to(device, { rotation: 10, duration: 0.2, ease: "elastic" })
+        .to(device, { rotation: 0, duration: 0.2, ease: "elastic" });
+    const { height, width } = canvas;
+
+    tween.play();
+    context.clearRect(0, 0, height, width);
+
+}
+
+clearBtn.addEventListener("click", shake);
